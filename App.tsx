f92ea43f65588da210a8d536c5ea80984e6df0b7
Thao198381 <157489934+Thao198381@users.noreply.github.com>
2026-04-10 15:29:13 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -107,12 +107,12 @@ const App: React.FC = () => {
   const payload = {
     action: "submitExamMatrix", // Action riêng cho ma trận
     timestamp: new Date().toLocaleString('vi-VN'),
-    exams: activeExam.code,
+    exams: activeExam.examCode,
     sbd: activeStudent.sbd,
     name: activeStudent.name,
     class: activeStudent.class,
     tongdiem: results.score,
-    time: results.timeSpent, // Thời gian làm bài (giây/phút)
+    time: result.time || 0,, // Thời gian làm bài (giây/phút)
     idgv: currentIDGV
   };
 
